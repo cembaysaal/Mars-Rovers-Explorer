67d762657a907aebb50bd2fe7bf7493cbf053aa1
Cem Baysal <cem.baysal@boyner.com.tr>
2024-06-09 16:19:05 +0300

    Application icon added
    Added photos of rovers for first page
    
    Network Manager class created

--- Mars-Rovers-Explorer/Mars-Rovers-Explorer/Networking/NetworkManager.swift
+++ Mars-Rovers-Explorer/Mars-Rovers-Explorer/Networking/NetworkManager.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+
+    func fetchRovers(completion: @escaping ([Rover]?) -> Void) {
+        let url = URL(string: "https://mars-photos.herokuapp.com/api/v1/rovers")!
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            let decoder = JSONDecoder()
+            do {
+                let roverResponse = try decoder.decode(RoverResponse.self, from: data)
+                completion(roverResponse.rovers)
+            } catch {
+                completion(nil)
+            }
+        }.resume()
+    }
+
+    func fetchLatestPhotos(for roverName: String, completion: @escaping ([Photo]?) -> Void) {
+        let urlString = "https://mars-photos.herokuapp.com/api/v1/rovers/\(roverName)/latest_photos"
+        let url = URL(string: urlString)!
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            let decoder = JSONDecoder()
+            do {
+                let photoResponse = try decoder.decode(PhotoResponse.self, from: data)
+                completion(photoResponse.latest_photos)
+            } catch {
+                completion(nil)
+            }
+        }.resume()
+    }
+}
